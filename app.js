56c24abf2c62d9ffeedbb8999380c85d0eb2f7a1
Kailee Smith <kaileesmith1501@gmail.com>
2020-10-17 23:44:23 -0700

    changes to ReadMe and updated to correct loop

--- app.js
+++ app.js
@@ -1,61 +1,70 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordopt =[];
 
-var upper= ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lower= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"]
+
+var uppercase= ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercase= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"]
 var nums = [1, 2, 4, 5, 6, 7, 8, 9, 0]
-var symbols = ["@", "$", "!", "(", "^"]
-var emojis = ["😀", "🤗", "😎", "⭐"]
+var symbol = ["@", "$", "!", "(", "^"]
+var emoji = ["😀", "🤗", "😎", "⭐"]
+
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+    
   passwordText.value = password;
   
 }
 
 
-
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
 generateBtn.addEventListener("click", writePassword);
+console.log("hello")
 
-
-// My Additions/
-
-var pLength = parseInt(prompt("How many characters would you like your password to be?"));
-if(pLength <= 8 || pLength >=128 || isNaN(length)) {
-  prompt("Length must be 8-128 characters. How many characters would you like your password to be?")
-  } else {
+// My Additions
+function generatePassword(){
+  var pLength = parseInt(prompt("How many characters would you like your password to be?"));
+  if(pLength < 8 || pLength >= 128 || isNaN(pLength)) {
+    alert("Length must be 8-128 characters. How many characters would you like your password to be?")
+    generatePassword()
+  }
+    else {
+    
+      var upper = confirm("Would you like to use uppercase letters?");
+      var lower = confirm("Would you like to use lowercase letters?");
+      var numbers = confirm("Would you like to use numbers?");
+      var symbols = confirm("Would you like to use special characters?");
+      var emojis = confirm("Do you want emojis?");
+      
+    }
+      if (upper) { passwordopt.push(uppercase) }
+      if (lower) { passwordopt.push(lowercase) }
+      if (numbers){ passwordopt.push(nums) }
+      if (symbols) { passwordopt.push(symbol) }
+      if (emojis) { passwordopt.push(emoji) }
   
-    var upper = confirm("Would you like to use uppercase letters?");
-    var lower = confirm("Would you like to use lowercase letters?");
-    var numbers = confirm("Would you like to use numbers?");
-    var symbols = confirm("Would you like to use special characters?");
-    var emojis = confirm("Do you want emojis?");
-
-// 
-    if (upper) password.push(upper)
-    if (lower) password.push(lower)
-    if (numbers) password.push(numbers)
-    if (symbols) { password.push(symbols) }
-    if (emojis) { password.push(emojis) }
-// 
+  
     var pw = ""
 
-
-    while (pw.length < length) {
-      for (var i=0; i < pLength.length; i++) {
-        if (pw.length < length) {
-          let rand = Math.floor(Math.random() * pLength[i].length)
-          pw += pLength[i][rand]
+  
+      while (pw.length < length) {
+  
+        for (var i = 0; i < passwordopt.length; i++) {
+          if (pw.length < length) {
+            let rand = Math.floor(Math.random() * passwordopt[i].length)
+            pw += passwordopt[i][rand]
+          
+          }
         }
-      }  
+        console.log(pw)
+        passwordText.textContent = pw
+      }
     }
-  }
-    document.querySelector("#generate").addEventListener("click", generateBtn)
-
-
-
+  
+  generatePassword()
+    document.querySelector("#password").addEventListener("click", generatePassword)
+    
